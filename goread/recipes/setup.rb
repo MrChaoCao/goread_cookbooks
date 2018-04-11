e026e3b32d89a943d43299eef842f415d08d283d
chao <mailbox.chao.cao@gmail.com>
2018-04-10 17:40:12 -0700

    Edit recipe setup file

--- goread/recipes/setup.rb
+++ goread/recipes/setup.rb
@@ -1,20 +1,46 @@
-default['ruby-ng']['ruby_version'] = "2.4"
-default['nodejs']['version'] = "6.10.1"
-default['nodejs']['npm']['version'] = "5.2.0"
-default['nginx']['default_site_enabled'] = false
+# setup script here
 
-default[:goread][:git_repository] = "https://github.com/MrChaoCao/goread/"
-default[:goread][:git_revision] = "master"
-default[:goread][:path] = "/opt/goread"
+include_recipe "ruby-ng::dev"
+include_recipe "nodejs"
+include_recipe "nodejs::npm"
+include_recipe "postgres"
+include_recipe "nginx"
+include_recipe "unicorn"
 
-default[:goread][:rails_env] = "production"
-default[:goread][:log_to_stdout] = "true"
+apt_package 'zlib1g-dev'
+apt_package 'libpq-dev'
 
-default[:goread][:environment] = {
-  "SECRET_KEY_BASE": node[:goread][:secret_key_base],
-  "DATABASE_URL": node[:goread][:database_url],
-  "RAILS_ENV": node[:goread][:rails_env],
-  "RAILS_LOG_TO_STDOUT": node[:goread][:log_to_stdout]
-}
+template "goread_service" do
+    path "/etc/init.d/goread"
+    source "goread_service.erb"
+    owner "root"
+    group "root"
+    mode "0755"
+end
 
-default[:goread][:start_cmd] = "unicorn -E production -c /opt/unicorn.rb"
+service "goread" do
+  supports :restart => true, :start => true, :stop => true, :reload => true
+  action [ :enable ]
+end
+
+template "#{node['nginx']['dir']}/sites-available/goread" do
+  source 'goread_site.erb'
+  notifies :reload, 'service[nginx]', :delayed
+end
+
+nginx_site 'goread' do
+  action :enable
+end
+
+directory '/tmp/sockets/' do
+  owner 'root'
+  group 'root'
+  mode '0777'
+  action :create
+end
+
+unicorn_config "/opt/unicorn.rb" do
+  listen ({"unix:/tmp/sockets/unicorn.sock": nil})
+  working_directory node[:goread][:path]
+  # /config/unicorn.rb
+end
